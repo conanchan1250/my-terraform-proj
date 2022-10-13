13b27d3fb9353b3a9cead4b9f5c5a6b03b0fe6c6
conanchan1250 <conanchan@live.ca>
2022-10-12 21:23:37 -0400

    Add project002 on VPC

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # my-terraform-proj
+
+## project001
+Example to setup user, create group, attach policy and add user to group
+
+## project002
+Create VPC.
+
+Basec on https://spacelift.io/blog/terraform-aws-vpc
---
+++ project002-vpc/main.tf
@@ -0,0 +1,80 @@
+// This will the following as in the default VPC
+// - VPC
+//   - default Route tables
+//   - default Network ACL
+//   - default Security group
+// - 6 subnets, 3 private, 3 public
+// - 1 Internet gateway
+// - 1 Second Route tables using Internet gateway
+
+// Step 1: create VPC
+//   When a VPC is created, the following is also created
+//   - a default Route table (without Internet Gateway) 
+//   - a default Network ACL is created.
+//   - a default Security group
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "Test VPC"
+  }
+}
+
+
+// Step 2: add subnets
+
+// Note, require to have same number of subnets and zones 
+resource "aws_subnet" "public_subnets" {
+  count             = length(var.public_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.public_subnet_cidrs, count.index)
+  availability_zone = element(var.azs, count.index)
+
+  tags = {
+    Name = "Public Subnet ${count.index + 1}"
+  }
+}
+
+// Note, require to have same number of subnets and zones 
+resource "aws_subnet" "private_subnets" {
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.private_subnet_cidrs, count.index)
+  availability_zone = element(var.azs, count.index)
+
+  tags = {
+    Name = "Private Subnet ${count.index + 1}"
+  }
+}
+
+
+// Step 3: add Internet Gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "Test VPC IG"
+  }
+}
+
+// Step 4: create second routing table using Internet gateway
+// Reason: default route table was created without Internet gateway.. Therefore nothing can reach internet.  
+resource "aws_route_table" "second_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "Route Table for Public"
+  }
+}
+
+// Step 5: Add public subnet to the second routing table
+resource "aws_route_table_association" "public_subnet_asso" {
+  count          = length(var.public_subnet_cidrs)
+  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+  route_table_id = aws_route_table.second_rt.id
+}
